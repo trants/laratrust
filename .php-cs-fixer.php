3a7870a0b288b65e0154a6fb2f05321cf50d2f82
Son Tran Thanh <286.trants@gmail.com>
2024-04-28 19:44:30 +0700

    Add the PHP-CS-Fixer configuration manager

---
+++ .php-cs-fixer.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * *************************************************************************
+ * Copyright (c) VSP Co., Ltd - All Rights Reserved
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ */
+
+// Directories to not scan
+$excludeDirs = [
+    'vendor/',
+];
+
+// Files to not scan
+$excludeFiles = [];
+
+// Create a new Symfony Finder instance
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude($excludeDirs)
+    ->ignoreDotFiles(true)->ignoreVCS(true)
+    ->filter(function (\SplFileInfo $file) use ($excludeFiles) {
+        return !in_array($file->getRelativePathName(), $excludeFiles);
+    });
+
+return (new \Vspc\PhpCsFixer\Config())
+    ->setFinder($finder)
+    ->setUsingCache(false)
+    ->setRiskyAllowed(true)
+    ->withPHPUnitRules();
